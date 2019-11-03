2ca87eb9268b5932007f3c778f360de253cab253
Dmitry <dmitry.ivoninsky@gmail.com>
2019-11-03 12:56:51 +0300

    Classwork2

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,85 @@
-import java.util.Arrays;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        /*int[] ints = new int[]{6, 2, 5, 1, 3};
-        ArraysSorted array = new ArraysSorted(ints);
-        System.out.println(array);
-        array.sort();
-        System.out.println(array);
-        Human human = new Human("Vasya", 11, "female");
-        Programmer programmer = new Programmer("Dmitry", 25, "male", "Java");
-        human.speak();
-        programmer.speak();*/
-        /*Animal animal = new Animal("Gentoo");
-        animal.speak();
-        animal.setName("IIII");
-        animal.speak();*/
+        /*String[] names = new String[]{"Vasya", "Petya"};
+        Map<Integer, String> map = new HashMap<>();
+        for (int i = 0; i < names.length; i++) {
+            map.put(i, names[i]);
+        }
+        System.out.println(map);
+
+        List<Integer> list = new ArrayList<>();
+        Random r = new Random();
+        int sum = 0;
+        for (int i = 0; i < 5; i++) {
+            list.add(r.nextInt(10));
+        }
+        System.out.println(list);
+        for (int i = 0; i < list.size(); i++) {
+            sum += list.get(i);
+        }
+        float average = (float) sum / list.size();
+        System.out.println("sum = " + sum + "; average = " + average);
+
+        List<String> list1= new ArrayList<>();
+        list1.add("RRR");
+        list1.add("TTT");
+        list1.add("YYY");
+        list1.add("TTT");
+        System.out.println(list1);
+        Set<String> set = new HashSet<>(list1);
+        for (String s : set) {
+            System.out.printf ("City: %s\t", s);
+
+        }
+
+        Map<String, Integer > map = new HashMap<>();
+        map.put("Minsk", 1_000_000);
+        map.put("Grodno", 900_000);
+        map.put("Mogilev", 800_000);
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            System.out.printf("City %s, population %d%n", entry.getKey(), entry.getValue());
+        }*/
+
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>();
+        set1.add(3);
+        set1.add(2);
+        set1.add(1);
+        set2.add(3);
+        set2.add(2);
+        set2.add(1);
+        set2.add(4);
+        boolean b = false;
+        if (set1.size() != set2.size()) {
+            b = false;
+        }
+        else {
+            for (Integer i : set1) {
+                if (!set2.contains(i)){
+                    b = false;
+                }
+            }
+            b = true;
+        }
+
+        System.out.println(b);
 
 
 
-        Dog dog = new Dog("jjj");
-        dog.speak();
-        dog.setName("likjfg;jk");
-        dog.speak();
+        //System.out.println(set1.equals(set2));
+
+
+
+
+
+
+
+
+
+
+
     }
+
 }
